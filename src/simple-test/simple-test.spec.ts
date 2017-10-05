3300000896a18d9a1a69a0c0ef5daebafb2cd161
Eugene Lukin <eugene.lukin@gmail.com>
2017-10-05 14:34:57 +0300

    Removed jasmine symbols error on test run

--- src/simple-test/simple-test.spec.ts
+++ src/simple-test/simple-test.spec.ts
@@ -2,5 +2,9 @@ import { simple } from './simple-test';
 describe('simple-test', () => {
     it('should succeed', () => {
         expect(simple(1)).toBe(2);
-    })
+    });
+    it('should succeed 2', () => {
+        expect(simple(2)).toBe(3);
+    });
+    
 });
